fd362494d79e4bac8d6c29e510b5a91f5cb43976
Brayan Esteves <werneriasearch@gmail.com>
2021-01-09 15:22:25 -0400

    0.0.1

---
+++ source/views/YOUTUBE/views/CHANNELS/FACULTAD AUTODIDACTA/06 - Gestor de archivos con php y mysql/31.- Sistema Gestor de Archivos - Mostrar y descargar archivos/assets/js/gestor.js
@@ -0,0 +1,46 @@
+function agregarArchivosGestor() {
+    var formData = new FormData(document.getElementById('frmArchivos'));
+        $.ajax({
+            url:"../../source/procesos/archivos/guardar.php",
+            type:"POST",
+            datatype: "html",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(respuesta) {
+                console.log(respuesta);
+                /**
+                 * print_r($_FILES); "procesos/archivos/guardar.php"
+                 * Array
+                 *  (
+                 *      [archivos] => Array
+                 *          (
+                 *              [name] => producto.xlsx
+                 *              [type] => application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+                 *              [tmp_name] => C:\wamp\tmp\phpC101.tmp
+                 *              [error] => 0
+                 *              [size] => 9900
+                 *          )
+                 *
+                 *  )
+                 * 
+                 * print_r($_POST); "procesos/archivos/guardar.php"
+                 * Array
+                 *  (
+                 *      [categoriasArchivos] => 2
+                 *  )
+                 */
+                respuesta = respuesta.trim();
+                if(respuesta == 1) {
+                    $('#tablaGestorArchivos').load("ajax/tabla.php");
+                    swal(":D", "Agregado con éxito!", "success");
+                } else if(respuesta == 2) { 
+                    swal("Advertencia", "Tipo de archivo no permitido", "warning");
+                }
+                else {
+                    swal(":(", "Falló al agregar !", "error");
+                }
+            }
+        });
+}
